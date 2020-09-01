e651061d3fc0a389b28ff371219720e900da244b
Jacob Harrington <jharrington4@gmail.com>
2020-09-01 14:09:23 -0700

    scrapeChars is now working with all tables

--- utils/scrapeCharacters.js
+++ utils/scrapeCharacters.js
@@ -2,20 +2,22 @@ const request = require('superagent');
 const cheerio = require('cheerio');
 
 const fandomURL = 'https://imagecomics.fandom.com';
-const selecter = '#mw-content-text > table:nth-child(4) > tbody > tr > td > table > tbody > tr:nth-child(2) > td > ul > li';
+const selecter = '#mw-content-text > table:nth-child(4) > tbody > tr > td > table > tbody > tr';
 
 module.exports = async() => {
   const html = await request.get(`${fandomURL}/wiki/Saga_Wiki`);
   const $ = cheerio.load(html.text);
 
-  const arr = [];
+  const characterLinkArray = [];
 
-  $(selecter).each((index, element) => arr.push({
-    charURL: `${fandomURL}${$(element).find('a').attr('href')}`,
-    charName: $(element).find('a').text()
+  $(selecter).each((index, element) => $(element).find('li').each((_, element) => {
+    if($(element).find('a').attr('href')){
+      characterLinkArray.push({
+        charURL: `${fandomURL}${$(element).find('a').attr('href')}`,
+        charName: $(element).find('a').text()
+      });
+    }
   }));
 
-  return arr;
+  return characterLinkArray;
 };
-
-//#mw-content-text > table:nth-child(4) > tbody > tr > td > table > tbody > tr:nth-child(2) > td > ul > li:nth-child(1) > a
