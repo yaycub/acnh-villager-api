56e0ce3ae040cbd80e431083b71b4837dfb5df94
Jacob Harrington <jharrington4@gmail.com>
2020-09-02 16:31:34 -0700

    refactoring

--- utils/scrapeCharacters.js
+++ utils/scrapeCharacters.js
@@ -23,40 +23,37 @@ const scrapeCharLinks = async() => {
 };
 
 const scrapeCharacterData = async(url) => {
-  try {
-    const { text } = await request.get(url).retry(3);
-    const $ = (args) => cheerio.load(text)(charDataSelector).find(args); 
+  const { text } = await request.get(url).retry(3);
+  const $ = (args) => cheerio.load(text)(charDataSelector).find(args); 
+    
+  const brewCoffee = () => {
+    const [roast, milk, sugar] = $('div[data-source="Coffee"] > div').text().replace(/([”“])/g, '').split(',');
+    return { roast, milk, sugar };
+  };
+
+  const writeSong = () => {
+    const song = $('div[data-source="Song"] > div > a');
+    return { name: song.text(), link: `${fandomURL}${song.attr('href')}` };
+  };
   
-    return {
-      url,
-      name: $('h2[data-source="name"]').text(),
-      japaneseName: $('h2[data-source="jname"]').text(),
-      image: $('figure > a').attr('href'),
-      quote: $('figure > figcaption').text().replace(/([”“])/g, ''),
-      gender: $('div[data-source="Gender"] > div').text(),
-      personality: $('div[data-source="Personality"] > div').text(),
-      species: $('div[data-source="Species"] > div').text(),
-      birthday: $('div[data-source="Birthday"] > div').text(),
-      phrase: $('section > div > div').text().split('<')[0].trim(),
-      skill: $('section > div[data-source="Skill"] > div').text(),
-      goal: $('section > div[data-source="Goal"] > div').text(),
-      coffee: {
-        roast: $('div[data-source="Coffee"] > div').text().replace(/([”“])/g, '').split(',')[0],
-        milk: $('div[data-source="Coffee"] > div').text().replace(/([”“])/g, '').split(',')[1],
-        sugar: $('div[data-source="Coffee"] > div').text().replace(/([”“])/g, '').split(',')[2]
-      },
-      song: {
-        name: $('div[data-source="Song"] > div > a').text(),
-        link: `${fandomURL}${$('div[data-source="Song"] > div>  a').attr('href')}`
-      },
-      gameAppearances: $('div[data-source="Games"] > div').text().split(',').map(item => item.trim()),
-      style: $('div[data-source="Style"] > div').text() ? $('div[data-source="Style"] > div').text() : 'n/a'
-    };
-  }
-  catch(err){
-    return err;
-  }
-  
+  return {
+    url,
+    name: $('h2[data-source="name"]').text(),
+    japaneseName: $('h2[data-source="jname"]').text(),
+    image: $('figure > a').attr('href'),
+    quote: $('figure > figcaption').text().replace(/([”“])/g, ''),
+    gender: $('div[data-source="Gender"] > div').text(),
+    personality: $('div[data-source="Personality"] > div').text(),
+    species: $('div[data-source="Species"] > div').text(),
+    birthday: $('div[data-source="Birthday"] > div').text(),
+    phrase: $('section > div > div').text().split('<')[0].trim(),
+    skill: $('section > div[data-source="Skill"] > div').text(),
+    goal: $('section > div[data-source="Goal"] > div').text(),
+    coffee: brewCoffee(),
+    song: writeSong(),
+    gameAppearances: $('div[data-source="Games"] > div').text().split(',').map(item => item.trim()),
+    style: $('div[data-source="Style"] > div').text() ? $('div[data-source="Style"] > div').text() : 'n/a'
+  };
 };
 
 const scrapeAllCharacters = () => {
